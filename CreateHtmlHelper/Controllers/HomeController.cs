bc3b6a0da8175fd4325b612650b24d5d5d9eec33
GreedNeSS <ruslan199126@gmail.com>
2022-07-08 19:25:45 +0300

    Create a new HTMLhelper with TagBuilder.

---
+++ CreateHtmlHelper/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CreateHtmlHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<string> people = new List<string>()
+        {
+            "GreedNeSS",
+            "Marcus",
+            "Henry"
+        };
+
+        public IActionResult Index()
+        {
+            return View(people);
+        }
+    }
+}
---
+++ CreateHtmlHelper/Helpers/ListHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Encodings.Web;
+using System;
+
+namespace CreateHtmlHelper.Helpers
+{
+    public static class ListHelper
+    {
+        public static HtmlString SimpleCreateList(this IHtmlHelper html, IEnumerable<string> items)
+        {
+            string result = "<ul>";
+
+            foreach (string item in items)
+            {
+                result = $"{result}<li>{item}</li>";
+            }
+
+            return new HtmlString($"{result}</ul>");
+        }
+
+        public static HtmlString CreateListWithTagBuilder(this IHtmlHelper html, IEnumerable<string> items)
+        {
+            TagBuilder ul = new TagBuilder("ul");
+
+            foreach (string item in items)
+            {
+                TagBuilder li = new TagBuilder("li");
+                li.InnerHtml.Append(item);
+                ul.InnerHtml.AppendHtml(li);
+            }
+
+            ul.Attributes.Add("class", "itemsList");
+            using var writer = new StringWriter();
+            ul.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
