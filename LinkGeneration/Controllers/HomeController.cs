744f4e90231efcdac87632e26378c3d120bb64cf
GreedNeSS <ruslan199126@gmail.com>
2022-07-11 19:41:00 +0300

    Added example of link generation.

---
+++ LinkGeneration/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LinkGeneration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About(int? id)
+        {
+            if (id is not null)
+            {
+                ViewBag.Id = id;
+            }
+
+            return View();
+        }
+
+        public IActionResult Users(int? id, string name)
+        {
+            if (id is not null)
+            {
+                ViewBag.Id = id;
+                ViewBag.Name = name;
+            }
+
+            return View();
+        }
+    }
+}
